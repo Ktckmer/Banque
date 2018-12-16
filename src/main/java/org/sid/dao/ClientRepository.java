9cc75a5eaf1008e43ce2848ea17c34ffa2b3c1c6
Cedrick <kamsicdrick@gmail.com>
2018-12-17 00:04:51 +0100

    Ajout de spring data rest pour tester avec l'API

--- src/main/java/org/sid/dao/ClientRepository.java
+++ src/main/java/org/sid/dao/ClientRepository.java
@@ -2,7 +2,9 @@ package org.sid.dao;
 
 import org.sid.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ClientRepository extends JpaRepository<Client, Long>{
 
 }
