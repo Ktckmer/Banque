4c33b4a806f900db7ade6ecd3b889b35788e7ee0
Cedrick <kamsicdrick@gmail.com>
2018-12-16 23:52:32 +0100

    Ajout des tests avec CommandLineRunner

--- src/main/java/org/sid/BanqueApplication.java
+++ src/main/java/org/sid/BanqueApplication.java
@@ -1,7 +1,16 @@
 package org.sid;
 
+import java.util.Date;
+
 import org.sid.dao.ClientRepository;
+import org.sid.dao.CompteRepository;
+import org.sid.dao.OperationRepository;
 import org.sid.entities.Client;
+import org.sid.entities.Compte;
+import org.sid.entities.CompteCourant;
+import org.sid.entities.CompteEpargne;
+import org.sid.entities.Retrait;
+import org.sid.entities.Versement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +22,12 @@ public class BanqueApplication implements CommandLineRunner {
 	@Autowired
 	private ClientRepository clientRepository;
 	
+	@Autowired
+	private CompteRepository compteRepository;
+	
+	@Autowired
+	private OperationRepository operationRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BanqueApplication.class, args);
 	}
@@ -23,7 +38,20 @@ public class BanqueApplication implements CommandLineRunner {
 		Client c1 = clientRepository.save(new Client("Cedrick", "kamsicdrick@gmail.com"));
 		Client c2 = clientRepository.save(new Client("Corine", "corine@cedrick.fr"));
 		Client c3 = clientRepository.save(new Client("Boris", "boris@cedrick.fr"));
-				
+		
+		Compte cp1 = compteRepository.save(new CompteCourant("c1", new Date(), 90000, c1, 6000));
+		Compte cp2 = compteRepository.save(new CompteEpargne("c2", new Date(), 6000, c2, 5.5));
+		
+		operationRepository.save(new Versement(new Date(), 9000, cp1));
+		operationRepository.save(new Versement(new Date(), 6000, cp1));
+		operationRepository.save(new Versement(new Date(), 2300, cp1));
+		operationRepository.save(new Retrait(new Date(), 9000, cp1));
+
+		operationRepository.save(new Versement(new Date(), 2300, cp2));
+		operationRepository.save(new Versement(new Date(), 6000, cp2));
+		operationRepository.save(new Versement(new Date(), 2300, cp2));
+		operationRepository.save(new Retrait(new Date(), 9000, cp2));
+		
 	}
 }
 
